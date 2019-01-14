a306d6ef6c4c98657a20a6da9ad7ef6a86823ad1
zhaopengxiang <980919517@qq.com>
2019-01-14 19:19:23 +0800

    修改样式

--- app/src/main/java/com/example/maiitzhao/myapplication/ShareElementActivity.java
+++ app/src/main/java/com/example/maiitzhao/myapplication/ShareElementActivity.java
@@ -1,5 +1,8 @@
 package com.example.maiitzhao.myapplication;
 
+import android.os.Build;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +19,8 @@ import butterknife.BindView;
  */
 public class ShareElementActivity extends BaseActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @BindView(R.id.iv_trans)
     ImageView ivTrans;
     @BindView(R.id.tv_trans)
@@ -32,5 +37,19 @@ public class ShareElementActivity extends BaseActivity {
         if (resId != 0) {
             ivTrans.setImageResource(resId);
         }
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar.setNavigationIcon(R.mipmap.ic_back);
+        toolbar.setTitle("共享元素");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
